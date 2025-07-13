8606b0b34dfc3a91b048fbc076d4b4008e2e8590
Asif Anam <anam1828@cseku.ac.bd>
2025-07-13 12:03:51 +0600

    initial commit

--- src/main/java/org/asifanam/photoappdiscoveryservice/PhotoAppDiscoveryServiceApplication.java
+++ src/main/java/org/asifanam/photoappdiscoveryservice/PhotoAppDiscoveryServiceApplication.java
@@ -2,12 +2,14 @@ package org.asifanam.photoappdiscoveryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class PhotoAppDiscoveryServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PhotoAppDiscoveryServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoAppDiscoveryServiceApplication.class, args);
+    }
 
 }
--- src/test/java/org/asifanam/photoappdiscoveryservice/PhotoAppDiscoveryServiceApplicationTests.java
+++ src/test/java/org/asifanam/photoappdiscoveryservice/PhotoAppDiscoveryServiceApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class PhotoAppDiscoveryServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
